f9b4af1475a031b6cb0aefab2682c4ba5f6f87ca
Jihad Ahmed <jihadahmed2004@gmail.com>
2023-10-02 16:02:11 +0600

    login,registration updated

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,100 +1,110 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { Link } from "react-router-dom";
-import auth from "../../Firebase/firebase.config";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const [registerError, setRegisterError] = useState("");
-  const [success, setSuccess] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+    const [registerError, setRegisterError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-  const handleRegister = (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    console.log(email, password);
+    const handleRegister = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const accepted = e.target.terms.checked;
+        console.log(name, email, password, accepted);
 
-    // reset error and success
-    setRegisterError("");
-    setSuccess("");
+        // reset error and success
+        setRegisterError('');
+        setSuccess('');
 
-    if (password.length < 6) {
-      setRegisterError("Password should be at least 6 characters or longer");
-      return;
+
+        if (password.length < 6) {
+            setRegisterError('Password should be at least 6 characters or longer');
+            return;
+        }
+        else if (!/[A-Z]/.test(password)) {
+            setRegisterError('Your password should have at least one upper case characters.')
+            return;
+        }
+        else if(!accepted){
+            setRegisterError('Please accept our terms and conditions!')
+            return;
+        }
+
+
+
+        // create user
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+                setSuccess('User Created Successfully.')
+
+                // update profile
+                updateProfile(result.user, {
+                    displayName: name, 
+                    photoURL: "https://example.com/jane-q-user/profile.jpg"
+                })
+                .then( () => console.log('profile updated'))
+                .catch()
+
+                // send verification email: 
+                sendEmailVerification(result.user)
+                .then( () =>{
+                    alert('Please check your email and verify your account')
+                })
+
+            })
+            .catch(error => {
+                console.error(error);
+                setRegisterError(error.message);
+            })
     }
-     else if (!/[A-Z]/.test(password)) {
-      setRegisterError(
-        "Your password should have at least one upper case characters."
-      );
-      return;
-    }
 
-    // create user
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        console.log(result.user);
-        setSuccess("User Created Successfully.");
-      })
-      .catch((error) => {
-        console.error(error);
-        setRegisterError(error.message);
-      });
-  };
+    return (
+        <div className="">
+            <div className="mx-auto md:w-1/2">
+                <h2 className="text-3xl mb-8">Please Register</h2>
+                <form onSubmit={handleRegister}>
+                    <input className="mb-4 w-full  py-2 px-4" type="text" name="name" placeholder="Your Name" id="" required />
+                    <br />
+                    <input className="mb-4 w-full  py-2 px-4" type="email" name="email" placeholder="Email Address" id="" required />
+                    <br />
+                    <div className="mb-4 relative border">
+                        <input
+                            className="w-full py-2 px-4"
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            placeholder="Password"
+                            id="" required />
+                        <span className="absolute top-3 right-2" onClick={() => setShowPassword(!showPassword)}>
+                            {
+                                showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                            }
 
-  return (
-    <div className="">
-      <div className="mx-auto md:w-1/2">
-        <h2 className="text-3xl mb-8">Please Register</h2>
-        <form onSubmit={handleRegister}>
-          {/* <input className="mb-4 w-full  py-2 px-4" type="text" name="name" placeholder="Your Name" id="" required />
-                    <br /> */}
-          <input
-            className="mb-4 w-full border py-2 px-4"
-            type="email"
-            name="email"
-            placeholder="Email Address"
-            id=""
-            required
-          />
-          <br />
-          <div className="mb-4 relative border">
-            <input
-              className="w-full py-2 px-4"
-              name="password"
-              placeholder="Password"
-              type={showPassword ? "text" : "password"}
-              id=""
-              required
-            />
-            <span className="absolute top-3 right-2" onClick={ () => setShowPassword(!showPassword)}>
+                        </span>
+                    </div>
+                    <br />
+                    <div className="mb-2">
+                        <input type="checkbox" name="terms" id="terms" />
+                        <label className="ml-2" htmlFor="terms">Accept our <a href="">Terms and Conditions</a></label>
+                    </div>
+                    <br />
+                    <input className="btn btn-secondary mb-4 w-full" type="submit" value="Register" />
+                </form>
                 {
-                    showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                    registerError && <p className="text-red-700">{registerError}</p>
                 }
-            </span>
-          </div>
-          <br />
-          <div className="mb-2">
-            <input type="checkbox" name="terms" id="terms" />
-            <label className="ml-2" htmlFor="terms">
-              Accept our <a href="">Terms and Conditions</a>
-            </label>
-          </div>
-          <br />
-          <input
-            className="btn btn-secondary mb-4 w-full"
-            type="submit"
-            value="Register"
-          />
-        </form>
-        {registerError && <p className="text-red-700">{registerError}</p>}
-        {success && <p className="text-green-600">{success}</p>}
-        <p>
-          Already have an account? Please <Link to="/login">Register</Link>
-        </p>
-      </div>
-    </div>
-  );
+                {
+                    success && <p className="text-green-600">{success}</p>
+                }
+                <p>Already have an account? Please <Link to="/login">Login</Link></p>
+            </div>
+        </div>
+    );
 };
 
 export default Register;
